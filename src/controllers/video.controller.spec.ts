64817f4556db6146f8233b3040a43264afd061e9
Adrian L Thomas <adrian.thomas@gmail.com>
2018-04-21 14:14:49 +0100

    Started tests for startVideo action

--- src/controllers/video.controller.spec.ts
+++ src/controllers/video.controller.spec.ts
@@ -1,8 +1,40 @@
 import { expect } from "chai";
+import * as http from "http";
+import * as sinon from "sinon";
+
+import { VideoController } from "./video.controller";
 
 describe("VideoController", () => {
-	it("startVideo: increments watch count");
-	it("startVideo: is not incremented higher than three");
+	let controller: VideoController;
+	let req: any = {};
+	let res: any = {};
+
+	beforeEach(function() {
+		controller = new VideoController();
+		req = { headers: sinon.spy() };
+		res = { writeHead: sinon.spy(), end: sinon.spy() };
+	});
+
+	it("startVideo: increments watch count by one", function() {
+		const initialWatchCount = controller.watchCount;
+		const expectedWatchCount = initialWatchCount + 1;
+		controller.startVideo(req, res);
+		const currentWatchCount = controller.watchCount;
+
+		expect(currentWatchCount).to.be.equal(expectedWatchCount);
+	});
+
+	it("startVideo: is not incremented higher than three", function() {
+		const expectedLimit = 3;
+		const startVideoTimes = 5;
+
+		for (let i = 0; i < startVideoTimes; i++) {
+			controller.startVideo(req, res);
+		}
+
+		expect(controller.watchCount).to.be.equal(expectedLimit);
+	});
+
 	it("startVideo: returns status code 200 when watch count is less than or equal to three");
 	it("startVideo: returns status code 403 when watch count is three");
 	it("endVideo: decrements watch count");
--- src/controllers/video.controller.ts
+++ src/controllers/video.controller.ts
@@ -1,9 +1,20 @@
+import * as http from "http";
 export class VideoController {
 	public watchCount = 0;
+	public readonly watchLimit = 3;
+	private readonly headers: http.OutgoingHttpHeaders;
 
-	public startVideo(req, res): void {
-		this.watchCount++;
-		res.writeHead(200);
-		res.end("OK " + this.watchCount);
+	constructor() {
+		this.headers = {"Content-Type": "text/plain"};
+	}
+
+	public startVideo(req: http.ServerRequest, res: http.ServerResponse): void {
+		if (this.watchCount < this.watchLimit) {
+			this.watchCount++;
+			// res.writeHead(200, this.headers);
+			// res.end("OK " + this.watchCount);
+		}
+
+		// fail
 	}
 }
