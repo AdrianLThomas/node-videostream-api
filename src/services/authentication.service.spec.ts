972bf634c585a1f8f5bc3c2e8d3ac84bc1a9d1ec
Adrian L Thomas <adrian.thomas@gmail.com>
2018-04-21 11:42:54 +0100

    Removed authentication tests

--- src/services/authentication.service.spec.ts
+++
@@ -1,9 +0,0 @@
-import * as assert from "assert";
-import { should } from "chai";
-import { expect } from "chai";
-should();
-
-describe("AuthenticationService", () => {
-	it("IsAuthenticated: returns true if any string is provided");
-	it("IsAuthenticated: returns false if nothing is provided");
-});
