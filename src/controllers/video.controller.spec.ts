845f81f0f5028fef682ae5062c4fd77422fd70e4
Adrian L Thomas <adrian.thomas@gmail.com>
2018-04-21 10:03:34 +0100

    outlined more tests

--- src/controllers/video.controller.spec.ts
+++ src/controllers/video.controller.spec.ts
@@ -4,8 +4,13 @@ import { expect } from "chai";
 should();
 
 describe("video controller", () => {
-	it("start video increments watch count");
-	it("start video is not incremented higher than three");
-	it("end video decrements watch count");
-	it("end video is not decremented less than zero");
+	it("startVideo: increments watch count");
+	it("startVideo: is not incremented higher than three");
+	it("startVideo: returns status code 200 when watch count is less than or equal to three");
+	it("startVideo: returns status code 403 when watch count is three");
+	it("endVideo: decrements watch count");
+	it("endVideo: is not decremented less than zero");
+	it("endVideo: returns status code 200 when watch count is more than or equal to zero");
+	it("endVideo: returns status code 403 when watch count is zero");
+	it("count: returns watch count with status code 200");
 });
