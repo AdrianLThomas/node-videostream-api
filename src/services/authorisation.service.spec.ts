65d373c070b8926974b5c1e31b34e07eea084083
Adrian L Thomas <adrian.thomas@gmail.com>
2018-04-21 11:41:59 +0100

    renamed to authorised, as authentication will not be happening for this demo

---
+++ src/services/authorisation.service.spec.ts
@@ -0,0 +1,9 @@
+import * as assert from "assert";
+import { should } from "chai";
+import { expect } from "chai";
+should();
+
+describe("AuthenticationService", () => {
+	it("IsAuthenticated: returns true if any string is provided");
+	it("IsAuthenticated: returns false if nothing is provided");
+});
