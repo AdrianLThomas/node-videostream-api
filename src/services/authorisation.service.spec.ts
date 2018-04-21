4387567b1973363385483bf5a84502e99ee52de4
Adrian L Thomas <adrian.thomas@gmail.com>
2018-04-21 11:42:21 +0100

    Renamed tests too

--- src/services/authorisation.service.spec.ts
+++ src/services/authorisation.service.spec.ts
@@ -3,7 +3,8 @@ import { should } from "chai";
 import { expect } from "chai";
 should();
 
-describe("AuthenticationService", () => {
-	it("IsAuthenticated: returns true if any string is provided");
-	it("IsAuthenticated: returns false if nothing is provided");
+describe("AuthorisationService", () => {
+	it("IsAuthorised: returns true if any string is provided");
+
+	it("IsAuthorised: returns false if nothing is provided");
 });
