34d136f5be8983ca5e7582aeecfc4d0c57a049ee
Adrian L Thomas <adrian.thomas@gmail.com>
2018-04-22 21:09:32 +0100

    Made lambda request object more consise

--- src/http/aws/lambda-request.ts
+++ src/http/aws/lambda-request.ts
@@ -2,8 +2,6 @@ import * as http from "http";
 import { IRequest } from "../request.interface";
 
 export class LambdaRequest implements IRequest {
-	constructor(private readonly internalHeaders: http.IncomingHttpHeaders) {
+	constructor(public readonly headers: http.IncomingHttpHeaders) {
 	}
-
-	public get headers(): http.IncomingHttpHeaders { return this.internalHeaders; }
 }
